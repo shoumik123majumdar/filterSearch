7a8d6e898fb3d90e56bfc5cd8c45d23fb6590e18
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-02-16 18:26:11 -0800

    Scrapped other import and decided to use selenium. Got it to run and apply the search terms. Will have to do some tweaking as far as the results go for the number of sites

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-import webbrowser
 from tkinter import*
-url = ""
-# MacOS
-chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+
+
+filter_list = []
+filter_list.append("youtube.com")
+filter_list.append("stackoverflow.com")
+filter_list.append("reddit.com")
 
 window = Tk()
 window.title("Web Search")
-window.minsize(width = 300,height=300)
-
-sites_label = Label(text="Enter the sites you want to filter for (seperated by commas)")
-sites_label.pack()
+window.minsize(width = 300,height=100)
 
 
-#webbrowser.get(chrome_path).open(url)
+search_label = Label(window,text="Enter your search query")
+search_label.pack()
+search_entry = Entry(window)
+search_entry.pack()
+
+
+
+def createSearchQuery(filter,query): return "site:" + filter + " " + query
+
+def search():
+    search_string = ""
+    for filter in filter_list:
+        search_string += createSearchQuery(filter,search_entry.get()) + " OR "
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.google.com/")
+    box = driver.find_element(By.NAME,"q")
+    box.send_keys(search_string)
+    box.send_keys(Keys.ENTER)
+
+clickButton = Button(window,text="Click to search",command=search)
+clickButton.pack()
+
+
 window.mainloop()
